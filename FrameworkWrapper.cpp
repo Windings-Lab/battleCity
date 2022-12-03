a2fbc05a75686173368fbc5bce7b395d4c8b41ca
Windings-Lab <janton20001@gmail.com>
2022-12-03 19:20:42 +0200

    Created empty version

--- FrameworkWrapper.cpp
+++ FrameworkWrapper.cpp
@@ -1,10 +1,10 @@
 #include "FrameworkWrapper.h"
 
-#include "GameManager.h"
-#include "WorldManager.h"
+// #include "GameManager.h"
+// #include "WorldManager.h"
 #include "Screen.h"
-#include "Sprites.h"
-#include "TankPlayer.h"
+// #include "Sprites.h"
+// #include "TankPlayer.h"
 
 namespace battleCity
 {
@@ -17,19 +17,21 @@ namespace battleCity
 
 	bool FrameworkWrapper::Init()
 	{
-		StartAllManagers();
+		// StartAllManagers();
 		return true;
 	}
 
 	void FrameworkWrapper::Close()
 	{
-		ShutDownAllManagers();
+		// ShutDownAllManagers();
 	}
 
 	bool FrameworkWrapper::Tick()
 	{
-		GM.Update();
-		return GM.GetGameOver();
+		// GM.Update();
+		// return GM.GetGameOver();
+
+		return false;
 	}
 
 	const char* FrameworkWrapper::GetTitle()
@@ -39,59 +41,59 @@ namespace battleCity
 
 	void FrameworkWrapper::onKeyPressed(FRKey k)
 	{
-		if (mPlayer->GetHealth() > 0)
-		{
-			mEventKeyboard.SetKey(k);
-			mEventKeyboard.SetKeyboardAction(EventKeyboardAction::KEY_PRESSED);
-			mPlayer->movementSet(k);
-			mPlayer->EventHandler(mEventKeyboard);
-		}
+		// if (mPlayer->GetHealth() > 0)
+		// {
+		// 	mEventKeyboard.SetKey(k);
+		// 	mEventKeyboard.SetKeyboardAction(EventKeyboardAction::KEY_PRESSED);
+		// 	mPlayer->movementSet(k);
+		// 	mPlayer->EventHandler(mEventKeyboard);
+		// }
 	}
 
 	void FrameworkWrapper::onKeyReleased(FRKey k)
 	{
-		if (mPlayer->GetHealth() > 0)
-		{
-			mEventKeyboard.SetKey(k);
-			mEventKeyboard.SetKeyboardAction(EventKeyboardAction::KEY_RELEASED);
-			mPlayer->movementErase(k);
-			mPlayer->EventHandler(mEventKeyboard);
-		}
+		// if (mPlayer->GetHealth() > 0)
+		// {
+		// 	mEventKeyboard.SetKey(k);
+		// 	mEventKeyboard.SetKeyboardAction(EventKeyboardAction::KEY_RELEASED);
+		// 	mPlayer->movementErase(k);
+		// 	mPlayer->EventHandler(mEventKeyboard);
+		// }
 	}
 
 	void FrameworkWrapper::onMouseMove(int x, int y, int xrelative, int yrelative)
 	{
-		mEventMouse.SetMousePosition({ static_cast<float>(x), static_cast<float>(y) });
+		// mEventMouse.SetMousePosition({ static_cast<float>(x), static_cast<float>(y) });
 	}
 
 	void FrameworkWrapper::onMouseButtonClick(FRMouseButton button, bool isReleased)
 	{
-		if (mPlayer->GetHealth() > 0)
-		{
-			mEventMouse.SetMouseButton(button);
-			mEventMouse.SetMouseAction(isReleased);
-			mPlayer->EventHandler(mEventMouse);
-		}
+		// if (mPlayer->GetHealth() > 0)
+		// {
+		// 	mEventMouse.SetMouseButton(button);
+		// 	mEventMouse.SetMouseAction(isReleased);
+		// 	mPlayer->EventHandler(mEventMouse);
+		// }
 	}
 
-	TankPlayer* GetPlayer(int id)
-	{
-		return dynamic_cast<TankPlayer*>(WM.GetAllObjects().mList.at(id).get());
-	}
+	// TankPlayer* GetPlayer(int id)
+	// {
+	// 	return dynamic_cast<TankPlayer*>(WM.GetAllObjects().mList.at(id).get());
+	// }
 
-	void FrameworkWrapper::StartAllManagers()
-	{
-		GM.StartUp();
-		WM.StartUp();
-
-		mPlayer = GetPlayer(WM.GetPlayerID());
-	}
-
-	void FrameworkWrapper::ShutDownAllManagers()
-	{
-		GM.ShutDown();
-		SPR.deleteAll();
-		WM.ShutDown();
-	}
+	// void FrameworkWrapper::StartAllManagers()
+	// {
+	// 	GM.StartUp();
+	// 	WM.StartUp();
+	//
+	// 	mPlayer = GetPlayer(WM.GetPlayerID());
+	// }
+	//
+	// void FrameworkWrapper::ShutDownAllManagers()
+	// {
+	// 	GM.ShutDown();
+	// 	SPR.deleteAll();
+	// 	WM.ShutDown();
+	// }
 
 }
