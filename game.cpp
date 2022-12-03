d7fa6ac8f72891cd75afed4f432eff66ab23e748
Windings-Lab <janton20001@gmail.com>
2022-12-03 22:45:51 +0200

    Main file cleanup

--- game.cpp
+++ game.cpp
@@ -22,27 +22,34 @@ namespace battleCity
 		int width = 800;
 		int height = 600;
 		bool fullScreen = false;
-		std::vector<int> tokens;
 		if (argc != 1)
 		{
-			std::vector <std::string> sources;
-			if (argc < 3) {
+			std::vector<int> tokens;
+			std::vector<std::string> sources;
+			if (argc < 3)
+			{
 				show_usage(argv[0]);
 				return 1;
 			}
 			std::string window;
-			for (int i = 1; i < argc; ++i) {
+			for (int i = 1; i < argc; ++i)
+			{
 				std::string arg = argv[i];
 				if ((arg == "-h") || (arg == "--help"))
 				{
 					show_usage(argv[0]);
 					return 0;
 				}
-				else if ((arg == "-w") || (arg == "-window")) {
-					if (i + 1 < argc) { // Make sure we aren't at the end of argv!
+				if ((arg == "-w") || (arg == "-window"))
+				{
+					if (i + 1 < argc)
+					{
+						// Make sure we aren't at the end of argv!
 						window = argv[i++]; // Increment 'i' so we don't get the argument as the next argv[i].
 					}
-					else { // Uh-oh, there was no argument to the window option.
+					else
+					{
+						// Uh-oh, there was no argument to the window option.
 						std::cerr << "--window option requires one argument." << std::endl;
 						return 1;
 					}
@@ -72,10 +79,11 @@ namespace battleCity
 			width = tokens[0];
 			height = tokens[1];
 		}
+
 		srand(time(nullptr));
 		Screen::Instance.Set(width, height, fullScreen);
 
-		auto framework = std::make_unique<battleCity::FrameworkWrapper>();
+		auto framework = std::make_unique<FrameworkWrapper>();
 
 		return run(framework.get());
 	}
