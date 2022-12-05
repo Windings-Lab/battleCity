d68495e8f17413e8b569c3009fecbb567a72b079
Windings-Lab <janton20001@gmail.com>
2022-12-05 19:47:54 +0200

    Declaration of SpriteManager

--- include/Manager/SpriteManager.h
+++ include/Manager/SpriteManager.h
@@ -1,12 +1,67 @@
 #pragma once
 
+#include <memory>
+#include <unordered_map>
+
+#include "Framework.h"
 #include "Manager.h"
+#include "Object.h"
 
 namespace BattleCity::Manager
 {
-	class SpriteManager : public Manager
+	enum class SpriteType;
+	enum class SpriteObjects;
+
+	//using SpriteAtlas = std::unordered_map<SpriteObjects, std::unordered_map<SpriteType, std::unique_ptr<Sprite>>>;
+
+	class SpriteManager final : public Manager
 	{
+		enum class SpriteType
+		{
+			Error,
+			Basic,
+			Down,
+			Left,
+			Right,
+			Up,
+			ExplosionL1,
+			ExplosionL2,
+			ExplosionS1,
+			ExplosionS2,
+			ExplosionS3
+		};
+
+		enum class SpriteObjects
+		{
+			Error,
+			TankPlayer = static_cast<int>(Object::Type::TankPlayer),
+			Tank = static_cast<int>(Object::Type::Tank),
+			Bullet = static_cast<int>(Object::Type::Bullet),
+			Wall = static_cast<int>(Object::Type::Wall),
+			Explosion = static_cast<int>(Object::Type::Explosion),
+			PowerUp = static_cast<int>(Object::Type::PowerUp),
+			Phoenix,
+			Flag,
+			Background,
+			GameOverLogo
+		};
+	public:
+		static SpriteManager& GetInstance();
+
+		SpriteManager(const SpriteManager&) = delete;
+		SpriteManager(SpriteManager&&) = delete;
+
+		SpriteManager& operator=(const SpriteManager&) = delete;
+		SpriteManager& operator=(SpriteManager&&) = delete;
+
+		~SpriteManager() override = default;
+
+		void StartUp() override;
+		void ShutDown() override;
+
 	private:
 		SpriteManager();
+
+		//SpriteAtlas mSpriteAtlas;
 	};
 }
---
+++ src/Manager/SpriteManager.cpp
@@ -0,0 +1,22 @@
+#include "SpriteManager.h"
+
+namespace BattleCity::Manager
+{
+	SpriteManager& SpriteManager::GetInstance()
+	{
+		static SpriteManager spriteManager;
+		return spriteManager;
+	}
+
+	SpriteManager::SpriteManager() : Manager(Type::Sprite)
+	{
+
+	}
+
+	void SpriteManager::StartUp()
+	{
+	}
+	void SpriteManager::ShutDown()
+	{
+	}
+}
