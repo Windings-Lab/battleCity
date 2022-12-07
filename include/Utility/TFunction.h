c2740f3a2d5871a90a219b3613579520f6e30947
Windings-Lab <janton20001@gmail.com>
2022-12-07 22:12:44 +0200

    Created Template Class for function pointer

--- include/Utility/TFunction.h
+++ include/Utility/TFunction.h
@@ -2,30 +2,62 @@
 
 namespace BattleCity
 {
-	template <typename FuncType, typename... ArgTypes>
+	template <typename ReturnType, typename... ArgsType>
 	class TFunction
 	{
+		typedef ReturnType(*FuncPtr)(ArgsType... args);
 	public:
-		explicit TFunction(FuncType(*func)(ArgTypes&))
-			: mFunc(func)
+		explicit TFunction(FuncPtr func)
+			: mFuncPtr(func)
 		{
 		}
 
 		TFunction(const TFunction& cpy) = delete;
-		TFunction(TFunction&& mve) noexcept : mFunc(std::move(mve.mFunc())) {}
-
+		TFunction(TFunction&& mve) noexcept : mFuncPtr(std::move(mve.mFuncPtr()))
+		{}
+		
 		TFunction& operator=(const TFunction&) = delete;
 		TFunction& operator=(TFunction&&) = delete;
-
+		
 		~TFunction() = default;
-
-		FuncType& operator()(ArgTypes... params)
+		
+		ReturnType operator()(ArgsType... params)
 		{
-			return mFunc(params...);
+			return mFuncPtr(params...);
 		}
 
 	private:
-		FuncType (*mFunc)(ArgTypes&... args);
+		FuncPtr mFuncPtr;
+	};
+
+	template <typename MType, typename ReturnType, typename... ArgsType>
+	class TFunctionMemeber
+	{
+		typedef ReturnType(MType::*FuncPtr)(ArgsType... args);
+	public:
+		explicit TFunctionMemeber(FuncPtr func, MType* obj)
+			: mFuncPtr(func), mObj(obj)
+		{
+		}
+
+		TFunctionMemeber(const TFunctionMemeber& cpy) = delete;
+		TFunctionMemeber(TFunctionMemeber&& mve) noexcept
+			: mFuncPtr(std::move(mve.mFuncPtr())), mObj(std::move(mve.mObj))
+		{}
+
+		TFunctionMemeber& operator=(const TFunctionMemeber&) = delete;
+		TFunctionMemeber& operator=(TFunctionMemeber&&) = delete;
+
+		~TFunctionMemeber() = default;
+
+		ReturnType operator()(ArgsType... params)
+		{
+			return (mObj->*mFuncPtr)(params...);
+		}
+
+	private:
+		FuncPtr mFuncPtr;
+		MType* mObj;
 	};
 }
 
