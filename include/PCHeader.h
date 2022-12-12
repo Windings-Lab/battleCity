09063d3239888d8122b77193ae794e4acc386553
Windings-Lab <janton20001@gmail.com>
2022-12-12 16:57:44 +0200

    Precompiled header sorted

--- include/PCHeader.h
+++ include/PCHeader.h
@@ -2,30 +2,38 @@
 
 #define FRAMEWORK_API extern "C" __declspec(dllimport)
 
-#include <algorithm>
-#include <array>
-#include <chrono>
-#include <cmath>
-#include <cstdlib>
+// String, Streams
 #include <filesystem>
 #include <fstream>
-#include <functional>
 #include <iostream>
-#include <magic_enum.hpp>
-#include <memory>
 #include <sstream>
 #include <string>
 #include <string_view>
-#include <thread>
-#include <type_traits>
+
+// Containers
+#include <array>
 #include <unordered_map>
 #include <unordered_set>
+
+// Exceptions
+#include <exception>
+#include <stdexcept>
+
+#include <algorithm>
+#include <chrono>
+#include <cmath>
+#include <cstdlib>
+#include <functional>
+#include <memory>
+#include <thread>
+#include <type_traits>
 #include <utility>
 
 using namespace std::string_view_literals;
 using namespace std::string_literals;
 
 #include "BattleCityEnums.h"
+#include "magic_enum.hpp"
 
 using SpriteTypeBehaviourPair = std::pair<BattleCity::Manager::SpriteType, BattleCity::Object::Behaviour>;
 
