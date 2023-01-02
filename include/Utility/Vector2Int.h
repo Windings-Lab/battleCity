a14ca0c9f5d0c86ee546addfe2c318a984eee9c1
Windings-Lab <janton20001@gmail.com>
2023-01-03 01:56:45 +0200

    Vector2int removed alias X, Y

--- include/Utility/Vector2Int.h
+++ include/Utility/Vector2Int.h
@@ -2,34 +2,33 @@
 
 namespace BattleCity
 {
-	using X = int;
-	using Y = int;
-
 	class Vector2Int final
 	{
 	public:
 		Vector2Int();
-		Vector2Int(int x, int y);
+		Vector2Int(int, int);
 
 		DISALLOW_COPY_MOVE(Vector2Int)
 
 		~Vector2Int() = default;
 
 		void SetXY(const Vector2Int& position);
-		void SetXY(X, Y);
+		void SetXY(int, int);
 
-		Vector2Int operator-(int num) const;
+		Vector2Int operator-(int) const;
 		Vector2Int operator-(const Vector2Int& other) const;
 
-		Vector2Int operator+(int num) const;
+		Vector2Int operator+(int) const;
 		Vector2Int operator+(const Vector2Int& other) const;
 
-		Vector2Int operator*(int num) const;
-		Vector2Int operator/(int num) const;
+		Vector2Int operator*(int) const;
+		Vector2Int operator/(int) const;
 
 		Vector2Int operator-() const;
+
 	public:
 		int X;
 		int Y;
+
 	};
 }
