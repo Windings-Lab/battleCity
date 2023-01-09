50918feea1adf10fa798525880ac937cf7062470
Windings-Lab <janton20001@gmail.com>
2023-01-09 19:48:17 +0200

    Vector2Int is now struct

--- include/BattleCity/Utility/Vector2Int.cpp
+++ include/BattleCity/Utility/Vector2Int.cpp
@@ -7,16 +7,6 @@ namespace BattleCity
     Vector2Int::Vector2Int() : Vector2Int(0, 0) {}
     Vector2Int::Vector2Int(int x, int y) : X(x), Y(y) {}
 
-    void Vector2Int::SetXY(const Vector2Int& position)
-    {
-        SetXY(position.X, position.Y);
-    }
-    void Vector2Int::SetXY(int x, int y)
-    {
-        X = x;
-        Y = y;
-    }
-
     Vector2Int Vector2Int::operator-(int num) const
     {
         return { X - num, Y - num };
--- include/BattleCity/Utility/Vector2Int.h
+++ include/BattleCity/Utility/Vector2Int.h
@@ -2,18 +2,10 @@
 
 namespace BattleCity
 {
-	class Vector2Int final
+	struct Vector2Int final
 	{
-	public:
 		Vector2Int();
 		Vector2Int(int, int);
-
-		ALLOW_COPY_MOVE_DEFAULT(Vector2Int)
-
-		~Vector2Int() = default;
-
-		void SetXY(const Vector2Int& position);
-		void SetXY(int, int);
 
 		Vector2Int operator-(int) const;
 		Vector2Int operator-(const Vector2Int& other) const;
@@ -26,9 +18,7 @@ namespace BattleCity
 
 		Vector2Int operator-() const;
 
-	public:
 		int X;
 		int Y;
-
 	};
 }
