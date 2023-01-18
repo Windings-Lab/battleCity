d36e1d1f6e677421e6ae2acf4f44df9eb458db3b
Windings-Lab <janton20001@gmail.com>
2023-01-19 00:18:21 +0200

    Vector2Int update
    All functions are noexcept
    Added operator== and operator!=

--- include/BattleCity/Utility/Vector2Int.cpp
+++ include/BattleCity/Utility/Vector2Int.cpp
@@ -7,36 +7,45 @@ namespace BattleCity
     Vector2Int::Vector2Int() : Vector2Int(0, 0) {}
     Vector2Int::Vector2Int(int x, int y) : X(x), Y(y) {}
 
-    Vector2Int Vector2Int::operator-(int num) const
+    Vector2Int Vector2Int::operator-(int num) const noexcept
     {
         return { X - num, Y - num };
     }
-    Vector2Int Vector2Int::operator-(const Vector2Int& other) const
+    Vector2Int Vector2Int::operator-(const Vector2Int& other) const noexcept
     {
         return { X - other.X, Y - other.Y };
     }
 
-    Vector2Int Vector2Int::operator+(int num) const
+    Vector2Int Vector2Int::operator+(int num) const noexcept
     {
         return { X + num, Y + num };
     }
-    Vector2Int Vector2Int::operator+(const Vector2Int& other) const
+    Vector2Int Vector2Int::operator+(const Vector2Int& other) const noexcept
     {
         return { X + other.X, Y + other.Y};
     }
 
-    Vector2Int Vector2Int::operator*(int num) const
+    Vector2Int Vector2Int::operator*(int num) const noexcept
     {
         return { X * num, Y * num };
     }
 
-    Vector2Int Vector2Int::operator/(int num) const
+    Vector2Int Vector2Int::operator/(int num) const noexcept
     {
         return { X / num, Y / num };
     }
 
-    Vector2Int Vector2Int::operator-() const
+    Vector2Int Vector2Int::operator-() const noexcept
     {
         return { -X, -Y };
+    }
+
+    bool Vector2Int::operator==(const Vector2Int& other) const noexcept
+    {
+        return X == other.X && Y == other.Y;
+    }
+    bool Vector2Int::operator!=(const Vector2Int& other) const noexcept
+    {
+	    return !(*this == other);
     }
 }
--- include/BattleCity/Utility/Vector2Int.h
+++ include/BattleCity/Utility/Vector2Int.h
@@ -7,16 +7,19 @@ namespace BattleCity
 		Vector2Int();
 		Vector2Int(int, int);
 
-		Vector2Int operator-(int) const;
-		Vector2Int operator-(const Vector2Int& other) const;
+		Vector2Int operator-(int) const noexcept;
+		Vector2Int operator-(const Vector2Int& other) const noexcept;
 
-		Vector2Int operator+(int) const;
-		Vector2Int operator+(const Vector2Int& other) const;
+		Vector2Int operator+(int) const noexcept;
+		Vector2Int operator+(const Vector2Int& other) const noexcept;
 
-		Vector2Int operator*(int) const;
-		Vector2Int operator/(int) const;
+		Vector2Int operator*(int) const noexcept;
+		Vector2Int operator/(int) const noexcept;
 
-		Vector2Int operator-() const;
+		Vector2Int operator-() const noexcept;
+
+		bool operator==(const Vector2Int&) const noexcept;
+		bool operator!=(const Vector2Int&) const noexcept;
 
 		int X;
 		int Y;
