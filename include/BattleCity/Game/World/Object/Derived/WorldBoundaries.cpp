ecc61172b73281379efc18e9ccbc4a715268c3db
Windings-Lab <janton20001@gmail.com>
2023-01-19 00:37:40 +0200

    WorldBoundaries updated to Object last updates

--- include/BattleCity/Game/World/Object/Derived/WorldBoundaries.cpp
+++ include/BattleCity/Game/World/Object/Derived/WorldBoundaries.cpp
@@ -3,6 +3,12 @@
 
 namespace BattleCity::Game::World::Object
 {
-
+    void WorldBoundaries::Update()
+    {
+    }
+    void WorldBoundaries::InitializeComponents()
+    {
+        Object::InitializeComponents();
+    }
 }
 
