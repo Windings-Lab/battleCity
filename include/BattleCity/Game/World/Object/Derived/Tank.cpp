1af3b91f0d3d2820b2e6a1fc75de7fdbacc38670
Windings-Lab <janton20001@gmail.com>
2023-01-22 22:21:07 +0200

    Tank implemented collision between movable objects

--- include/BattleCity/Game/World/Object/Derived/Tank.cpp
+++ include/BattleCity/Game/World/Object/Derived/Tank.cpp
@@ -28,7 +28,53 @@ namespace BattleCity::Game::World::Object
 
     void Tank::ResolveCollisions(Object& other)
     {
+        if (!mMovable->IsMoving()) return;
+        auto otherCollider = other.GetComponent<Component::Collider>();
+        auto otherMovable = other.GetComponent<Component::Movable>();
+        if (!otherCollider->IsSolid() || !otherMovable) return;
 
+        auto& rectangle = mCollider->GetRectangle();
+        auto& otherRectangle = otherCollider->GetRectangle();
+        auto& oldRectangle = mCollider->GetOldRectangle();
+        auto& oldOtherRectangle = otherCollider->GetOldRectangle();
+
+        auto penetration = rectangle.GetPenetration(otherRectangle);
+
+        auto movementDirection = mMovable->GetMovementDirection();
+
+        switch (movementDirection)
+        {
+        case Direction::Right:
+            if (oldRectangle.GetX() + oldRectangle.GetWidth() <= oldOtherRectangle.GetX())
+            {
+                SetX(rectangle.GetPosition().X + penetration.X);
+                mMovable->StopMovement();
+            }
+            break;
+        case Direction::Left:
+            if(oldRectangle.GetX() >= oldOtherRectangle.GetX() + oldOtherRectangle.GetWidth())
+            {
+                SetX(rectangle.GetPosition().X + penetration.X);
+                mMovable->StopMovement();
+            }
+            break;
+        case Direction::Down:
+            if (oldRectangle.GetY() + oldRectangle.GetHeight() <= oldOtherRectangle.GetY())
+            {
+                SetY(rectangle.GetPosition().Y + penetration.Y);
+                mMovable->StopMovement();
+            }
+            break;
+        case Direction::Up:
+            if (oldRectangle.GetY() >= oldOtherRectangle.GetY() + oldOtherRectangle.GetHeight())
+            {
+                SetY(rectangle.GetPosition().Y + penetration.Y);
+                mMovable->StopMovement();
+            }
+            break;
+        }
+
+        Object::ResolveCollisions(other);
     }
 
     void Tank::OnOutOfBounds(const Vector2Int& penetration)
