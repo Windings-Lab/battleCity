b6e2e0d1f6213e226f0c250518759aeeb1d1d756
Windings-Lab <janton20001@gmail.com>
2023-01-23 14:02:00 +0200

    TankNPC stop moving if collide with non movable object

--- include/BattleCity/Game/World/Object/Derived/Tank/TankNPC.cpp
+++ include/BattleCity/Game/World/Object/Derived/Tank/TankNPC.cpp
@@ -1,14 +1,60 @@
 #include "PCHeader.h"
 #include "TankNPC.h"
 
+#include "BattleCity/Game/World/Object/Components/Collider.h"
 #include "BattleCity/Game/World/Object/Components/Movable.h"
 
 namespace BattleCity::Game::World::Object
 {
-	void TankNPC::ResolveCollisions(Object& object)
+	void TankNPC::ResolveCollisions(Object& other)
 	{
-		Tank::ResolveCollisions(object);
-		mMovable->SetMovementDirection(mMovable->GetMovementDirection());
+        if (!mMovable->IsMoving()) return;
+        auto otherCollider = other.GetComponent<Component::Collider>();
+        auto otherMovable = other.GetComponent<Component::Movable>();
+        if (!otherCollider->IsSolid() || !otherMovable) return;
+
+        auto& rectangle = mCollider->GetRectangle();
+        auto& otherRectangle = otherCollider->GetRectangle();
+        auto& oldRectangle = mCollider->GetOldRectangle();
+        auto& oldOtherRectangle = otherCollider->GetOldRectangle();
+
+        auto penetration = rectangle.GetPenetration(otherRectangle);
+
+        auto movementDirection = mMovable->GetMovementDirection();
+
+        switch (movementDirection)
+        {
+        case Direction::Right:
+            // If Collision on Right
+            if (oldRectangle.GetX() + oldRectangle.GetWidth() <= oldOtherRectangle.GetX())
+            {
+                SetX(rectangle.GetPosition().X + penetration.X);
+            }
+            break;
+        case Direction::Left:
+            // If Collision on Left
+            if (oldRectangle.GetX() >= oldOtherRectangle.GetX() + oldOtherRectangle.GetWidth())
+            {
+                SetX(rectangle.GetPosition().X + penetration.X);
+            }
+            break;
+        case Direction::Down:
+            // If Collision on Down
+            if (oldRectangle.GetY() + oldRectangle.GetHeight() <= oldOtherRectangle.GetY())
+            {
+                SetY(rectangle.GetPosition().Y + penetration.Y);
+            }
+            break;
+        case Direction::Up:
+            // If Collision on Up
+            if (oldRectangle.GetY() >= oldOtherRectangle.GetY() + oldOtherRectangle.GetHeight())
+            {
+                SetY(rectangle.GetPosition().Y + penetration.Y);
+            }
+            break;
+        }
+
+        Object::ResolveCollisions(other);
 	}
 }
 
